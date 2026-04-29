cf55c938e47c87c93cfee46a0514440ae954cdff
sazzad-hosen-mahim <sajjadhosenmahim@gmail.com>
2026-04-30 02:01:12 +0600

    Zoniva text changed to Amazon

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return <div className="flex items-center justify-center h-screen">
-    <h1 className="text-5xl text-center font-bold">Zoniva Dashboard</h1>
+    <h1 className="text-5xl text-center font-bold">Amazon Dashboard</h1>
   </div>;
 };
 
